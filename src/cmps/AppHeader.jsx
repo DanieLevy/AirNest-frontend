a70c3d17041a8ab60613acad346ef5dbd0dbffe8
peleg125 <peleg@mischel.biz>
2023-10-03 18:28:38 +0300

    fix: file names

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -5,56 +5,58 @@ import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { login, logout, signup } from '../store/actions/user.actions.js'
 import { LoginSignup } from './LoginSignup.jsx'
 import { FilterBy } from './FilterBy'
+import { UserMsg } from './UserMsg'
 
 export function AppHeader() {
-    const user = useSelector(storeState => storeState.userModule.user)
+  const user = useSelector((storeState) => storeState.userModule.user)
 
-    async function onLogin(credentials) {
-        try {
-            const user = await login(credentials)
-            showSuccessMsg(`Welcome: ${user.fullname}`)
-        } catch (err) {
-            showErrorMsg('Cannot login')
-        }
+  async function onLogin(credentials) {
+    try {
+      const user = await login(credentials)
+      showSuccessMsg(`Welcome: ${user.fullname}`)
+    } catch (err) {
+      showErrorMsg('Cannot login')
     }
-    async function onSignup(credentials) {
-        try {
-            const user = await signup(credentials)
-            showSuccessMsg(`Welcome new user: ${user.fullname}`)
-        } catch (err) {
-            showErrorMsg('Cannot signup')
-        }
+  }
+  async function onSignup(credentials) {
+    try {
+      const user = await signup(credentials)
+      showSuccessMsg(`Welcome new user: ${user.fullname}`)
+    } catch (err) {
+      showErrorMsg('Cannot signup')
     }
-    async function onLogout() {
-        try {
-            await logout()
-            showSuccessMsg(`Bye now`)
-        } catch (err) {
-            showErrorMsg('Cannot logout')
-        }
+  }
+  async function onLogout() {
+    try {
+      await logout()
+      showSuccessMsg(`Bye now`)
+    } catch (err) {
+      showErrorMsg('Cannot logout')
     }
+  }
 
-    return (
-        <header className="app-header flex" style={{ justifyContent: 'space-between' }}>
-            <h1>AirBnB</h1>
-            <FilterBy />
-            <div>
-                {user &&
-                    <span className="user-info">
-                        <Link to={`user/${user._id}`}>
-                            {user.imgUrl && <img src={user.imgUrl} />}
-                            {user.fullname}
-                        </Link>
-                        <span className="score">{user.score?.toLocaleString()}</span>
-                        <button onClick={onLogout}>Logout</button>
-                    </span>
-                }
-                {!user &&
-                    <section className="user-info">
-                        <LoginSignup onLogin={onLogin} onSignup={onSignup} />
-                    </section>
-                }
-            </div>
-        </header>
-    )
+  return (
+    <header className='app-header flex' style={{ justifyContent: 'space-between' }}>
+      <h1>AirBnB</h1>
+      <FilterBy />
+      <div>
+        {user && (
+          <span className='user-info'>
+            <Link to={`user/${user._id}`}>
+              {user.imgUrl && <img src={user.imgUrl} />}
+              {user.fullname}
+            </Link>
+            <span className='score'>{user.score?.toLocaleString()}</span>
+            <button onClick={onLogout}>Logout</button>
+          </span>
+        )}
+        {!user && (
+          <section className='user-info'>
+            <LoginSignup onLogin={onLogin} onSignup={onSignup} />
+          </section>
+        )}
+      </div>
+      <UserMsg />
+    </header>
+  )
 }
