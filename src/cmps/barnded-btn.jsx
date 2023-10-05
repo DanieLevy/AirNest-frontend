7cfda73bcff55e4f0516007b63a9279055997ea2
Daniel Levy <daniellofficial@gmail.com>
2023-10-05 21:36:18 +0300

    ExploreBy starter design

--- src/cmps/barnded-btn.jsx
+++ src/cmps/barnded-btn.jsx
@@ -1,4 +1,4 @@
-export function BarndedBtn(props) {
+export function BarndedBtn({ txt, borderRadius = "8px", width = "100%" }) {
   return (
     <div className="btn-container">
       <div className="cell"></div>
@@ -101,9 +101,12 @@ export function BarndedBtn(props) {
       <div className="cell"></div>
       <div className="cell"></div>
       <div className="cell"></div>
-      <div className="content">
+      <div
+        className="content"
+        style={{ borderRadius: borderRadius, width: width }}
+      >
         <button className="action-btn">
-          <span className="branded-btn-txt">{props.txt}</span>
+          <span className="branded-btn-txt">{txt}</span>
         </button>
       </div>
     </div>
