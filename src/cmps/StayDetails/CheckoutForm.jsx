cbde33c3fec5705d40792f0c8bd4d8d2ab031707
peleg125 <peleg@mischel.biz>
2023-10-05 21:37:47 +0300

    Added brandedBtn to checkout

--- src/cmps/StayDetails/CheckoutForm.jsx
+++ src/cmps/StayDetails/CheckoutForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import DatePicker from 'react-datepicker'
+import { BarndedBtn } from '../barnded-btn'
 
 export function CheckoutForm({ onSubmit }) {
   const [startDate, setStartDate] = useState(Date.now())
@@ -64,7 +65,7 @@ export function CheckoutForm({ onSubmit }) {
         </label>
       </div>
       <div>
-        <button type='submit'>Checkout</button>
+        <BarndedBtn txt={'Checkout'} />
       </div>
     </form>
   )
