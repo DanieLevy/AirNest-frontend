ce0479b83a9fcfaf8db993e36b3592cb678b851e
Daniel Levy <daniellofficial@gmail.com>
2023-10-23 16:30:04 +0300

    More work on filter modal

--- src/cmps/StayFilter.jsx
+++ src/cmps/StayFilter.jsx
@@ -22,6 +22,10 @@ export function StayFilter() {
     "inset 0 0 0 1px #b0b0b0"
   );
 
+  const [selectedBedrooms, setSelectedBedrooms] = useState("Any");
+  const [selectedBeds, setSelectedBeds] = useState("Any");
+  const [selectedBathrooms, setSelectedBathrooms] = useState("Any");
+
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
@@ -96,112 +100,176 @@ export function StayFilter() {
         </section>
       </section>
 
-        <section className="filter-modal-container"
-        style={filterModal ? 
-          {transform: 'translateY(0)',
-          backgroundColor: 'rgba(0,0,0,0.5)',
-        } 
-        :
-         {transform: 'translateY(100%)',
-          backgroundColor: 'rgba(0,0,0,0)',
+      <section
+        className="filter-modal-container"
+        style={
+          filterModal
+            ? { transform: "translateY(0)", backgroundColor: "rgba(0,0,0,0.5)" }
+            : {
+                transform: "translateY(100%)",
+                backgroundColor: "rgba(0,0,0,0)",
+              }
         }
-      }
-        >
-          <div className="filter-modal">
-            <div className="filter-modal-header">
-              <div
-                className="close-btn"
-                onClick={() => {
-                  setFilterModal(false);
-                }}
-              >
-                &#10005;
-              </div>
-              <div className="filter-modal-title">Filters</div>
-              <div></div>
+      >
+        <div className="filter-modal">
+          <div className="filter-modal-header">
+            <div
+              className="close-btn"
+              onClick={() => {
+                setFilterModal(false);
+              }}
+            >
+              &#10005;
             </div>
-            <div className="filter-modal-body">
-              <div className="price-range">
-                <div className="price-range-title">Price range</div>
-                <div className="price-range-slider-container">
-                  <Slider
-                    range
-                    className="price-range-slider"
-                    min={0}
-                    max={1500}
-                    defaultValue={[0, 1000]}
-                    trackStyle={{ backgroundColor: "black", height: "2px" }}
-                    handleStyle={{
-                      backgroundColor: "white",
-                      border: "none",
-                      backgroundColor: "white",
-                      opacity: "1",
-                      outline: "none",
-                      boxShadow: "0 0 6px rgb(0 0 0 / 16%)",
-                      width: "20px",
-                      height: "20px",
-                      marginTop: "-9px",
-                    }}
-                    railStyle={{ backgroundColor: "#e4e4e4", height: "2px" }}
-                    onChange={onSliderChange}
-                  />
+            <div className="filter-modal-title">Filters</div>
+            <div></div>
+          </div>
+          <div className="filter-modal-body">
+            <div className="price-range">
+              <div className="price-range-title">Price range</div>
+              <div className="price-range-slider-container">
+                <Slider
+                  range
+                  className="price-range-slider"
+                  min={0}
+                  max={1500}
+                  defaultValue={[0, 1000]}
+                  trackStyle={{ backgroundColor: "black", height: "2px" }}
+                  handleStyle={{
+                    backgroundColor: "white",
+                    border: "none",
+                    backgroundColor: "white",
+                    opacity: "1",
+                    outline: "none",
+                    boxShadow: "0 0 6px rgb(0 0 0 / 16%)",
+                    width: "20px",
+                    height: "20px",
+                    marginTop: "-9px",
+                  }}
+                  railStyle={{ backgroundColor: "#e4e4e4", height: "2px" }}
+                  onChange={onSliderChange}
+                />
+              </div>
+              <div className="price-range-inputs">
+                <div
+                  className="price-range-input"
+                  style={{ boxShadow: inputBoxShadow1 }}
+                >
+                  <label htmlFor="min">Minimum</label>
+                  <div className="input-container flex align-center">
+                    <span>$</span>
+                    <input
+                      type="text"
+                      placeholder="Min"
+                      id="min"
+                      value={rangeValues[0]}
+                      onFocus={() => {
+                        setInputBoxShadow1("inset 0 0 0 2px #222");
+                      }}
+                      onBlur={() => {
+                        setInputBoxShadow1("inset 0 0 0 1px #b0b0b0");
+                      }}
+                    />
+                  </div>
                 </div>
-                <div className="price-range-inputs">
-                  <div
-                    className="price-range-input"
-                    style={{ boxShadow: inputBoxShadow1 }}
-                  >
-                    <label htmlFor="min">Minimum</label>
-                    <div className="input-container flex align-center">
-                      <span>$</span>
-                      <input
-                        type="text"
-                        placeholder="Min"
-                        id="min"
-                        value={rangeValues[0]}
-                        onFocus={() => {
-                          setInputBoxShadow1('inset 0 0 0 2px #222');
-                        }}
-                        onBlur={() => {
-                          setInputBoxShadow1('inset 0 0 0 1px #b0b0b0');
-                        }}
+                <div className="price-range-divider"></div>
+                <div
+                  className="price-range-input"
+                  style={{ boxShadow: inputBoxShadow2 }}
+                >
+                  <label htmlFor="max">Maximum</label>
+                  <div className="input-container flex align-center">
+                    <span>$</span>
+                    <input
+                      type="text"
+                      placeholder="Max"
+                      id="max"
+                      value={rangeValues[1]}
+                      onFocus={() => {
+                        setInputBoxShadow2("inset 0 0 0 2px #222");
+                      }}
+                      onBlur={() => {
+                        setInputBoxShadow2("inset 0 0 0 1px #b0b0b0");
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+              <div className="stay-details">
+                <div className="stay-details-title">Rooms and beds</div>
+                <div className="details-title">Bedrooms</div>
+                <div className="details-inputs">
+                  {["Any", "1", "2", "3", "4", "5", "6", "7", "8+"].map(
+                    (label) => (
+                      <div className="details-input" key={label}>
+                        <button
+                          className={`details-btn ${
+                            selectedBedrooms === label ? "selected" : ""
+                          }`}
+                          onClick={() => {
+                            console.log(label);
+                            setSelectedBedrooms(label);
+                          }}
+                        >
+                          <span>{label}</span>
+                        </button>
+                      </div>
+                    )
+                  )}
+                </div>
 
-                      />
-                    </div>
-                  </div>
-                  <div className="price-range-divider"></div>
-                  <div
-                    className="price-range-input"
-                    style={{ boxShadow: inputBoxShadow2 }}
-                  >
-                    <label htmlFor="max">Maximum</label>
-                    <div className="input-container flex align-center">
-                      <span>$</span>
-                      <input
-                        type="text"
-                        placeholder="Max"
-                        id="max"
-                        value={rangeValues[1]}
-                        onFocus={() => {
-                          setInputBoxShadow2('inset 0 0 0 2px #222');
-                        }}
-                        onBlur={() => {
-                          setInputBoxShadow2('inset 0 0 0 1px #b0b0b0');
-                        }} 
-                      />
-                    </div>
-                  </div>
+                <div className="details-title">Beds</div>
+                <div className="details-inputs">
+                  {["Any", "1", "2", "3", "4", "5", "6", "7", "8+"].map(
+                    (label) => (
+                      <div className="details-input" key={label}>
+                        <button
+                          className={`details-btn ${
+                            selectedBeds === label ? "selected" : ""
+                          }`}
+                          onClick={() => {
+                            console.log(label);
+                            setSelectedBeds(label);
+                          }}
+                        >
+                          <span>{label}</span>
+                        </button>
+                      </div>
+                    )
+                  )}
+                </div>
+
+                <div className="details-title">Bathrooms</div>
+                <div className="details-inputs">
+                  {["Any", "1", "2", "3", "4", "5", "6", "7", "8+"].map(
+                    (label) => (
+                      <div className="details-input" key={label}>
+                        <button
+                          className={`details-btn ${
+                            selectedBathrooms === label ? "selected" : ""
+                          }`}
+                          onClick={() => {
+                            console.log(label);
+                            setSelectedBathrooms(label);
+                          }}
+                        >
+                          <span>{label}</span>
+                        </button>
+                      </div>
+                    )
+                  )}
                 </div>
               </div>
             </div>
-            <div className="filter-modal-footer">
-              <div className="clear-btn">Clear all</div>
-              <div className="search-btn">
-                <button className="filter-modal-btn">Show 2 places</button>
-              </div>
+          <div className="filter-modal-footer">
+            <div className="clear-btn">Clear all</div>
+            <div className="search-btn">
+              <button className="filter-modal-btn">Show 2 places</button>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
     </React.Fragment>
   );
 }
