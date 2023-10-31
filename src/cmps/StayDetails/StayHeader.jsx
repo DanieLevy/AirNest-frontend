1ea53033f7b1297dcc1620c20a9e04a18dabf4fa
peleg125 <peleg@mischel.biz>
2023-10-31 14:06:47 +0200

    Fixed decimal point on whole numbers

--- src/cmps/StayDetails/StayHeader.jsx
+++ src/cmps/StayDetails/StayHeader.jsx
@@ -1,111 +1,110 @@
-import ImageGallery from "react-image-gallery";
-import { DetailsImages } from "./DetailsImages";
-import { BiHeart } from "react-icons/bi";
-import { useState } from "react";
-import { useEffect } from "react";
-import React from "react";
+import ImageGallery from 'react-image-gallery'
+import { DetailsImages } from './DetailsImages'
+import { BiHeart } from 'react-icons/bi'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import React from 'react'
 
-import { GoShare } from "react-icons/go";
-import { PiUploadSimpleLight } from "react-icons/pi";
-import { PiHeartLight } from "react-icons/pi";
-import { PiArrowLeftLight } from "react-icons/pi";
-import { useNavigate } from "react-router";
-import { store } from "../../store/store";
+import { GoShare } from 'react-icons/go'
+import { PiUploadSimpleLight } from 'react-icons/pi'
+import { PiHeartLight } from 'react-icons/pi'
+import { PiArrowLeftLight } from 'react-icons/pi'
+import { useNavigate } from 'react-router'
+import { store } from '../../store/store'
 
-import { AiFillStar } from "react-icons/ai";
-import { useDispatch } from "react-redux";
+import { AiFillStar } from 'react-icons/ai'
+import { useDispatch } from 'react-redux'
 
 export function StayHeader({ name, imgUrls, reviews, loc, host }) {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
+      setIsMobile(window.innerWidth < 768)
+    }
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize)
 
     return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  function calculateAverageRating(reviews) {
-    if (!reviews.length) return 0;
-
-    const totalRating = reviews.reduce((sum, review) => sum + review.rate, 0);
-
-    return totalRating / reviews.length;
-  }
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   const scrollToLocation = () => {
-    const element = document.querySelector('.details-map');
+    const element = document.querySelector('.details-map')
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: 'smooth' })
     }
-  };
+  }
+  const sumOfRatings = reviews.reduce((acc, review) => {
+    return acc + review.rate
+  }, 0)
 
-  const avgRate = calculateAverageRating(reviews).toFixed(2);
-  const isSuperhost = host.isSuperhost ? "Superhost" : "";
+  const rawAvg = sumOfRatings / reviews.length
+  const avgRating = rawAvg % 1 === 0 ? Math.floor(rawAvg) : rawAvg.toFixed(2)
+
+  const isSuperhost = host.isSuperhost ? 'Superhost' : ''
 
   return (
     <React.Fragment>
       {!isMobile && (
-        <div className="stay-header-container">
-          <div className="stay-header">
-            <h1 className="name-title">{name}</h1>
-            <div className="stay-header-top flex">
-              <div className="stay-details-rating">
+        <div className='stay-header-container'>
+          <div className='stay-header'>
+            <h1 className='name-title'>{name}</h1>
+            <div className='stay-header-top flex'>
+              <div className='stay-details-rating'>
                 <AiFillStar />
-                <span className="avg-rate">{avgRate}</span>
-                <span className="dot">·</span>
+                <span className='avg-rate'>{avgRating}</span>
+                <span className='dot'>·</span>
                 <a
-                  style={{ cursor: "pointer" }}
+                  style={{ cursor: 'pointer' }}
                   onClick={() => {
                     store.dispatch({
-                      type: "SET_REVIEWS_MODAL",
+                      type: 'SET_REVIEWS_MODAL',
                       reviewsModal: true,
-                    });
+                    })
                   }}
                 >
                   {reviews.length} Reviews
                 </a>
-                <span className="dot">·</span>
+                <span className='dot'>·</span>
                 {isSuperhost && (
-                  <div className="superhost">
-                    <div className="superhost-icon">󰀃</div>
-                    <span className="superhost-badge">{isSuperhost}<span className="dot">·</span></span>
+                  <div className='superhost'>
+                    <div className='superhost-icon'>󰀃</div>
+                    <span className='superhost-badge'>
+                      {isSuperhost}
+                      <span className='dot'>·</span>
+                    </span>
                   </div>
                 )}
-                <div className="location flex align-center"
-                  onClick={scrollToLocation}
-                >
+                <div className='location flex align-center' onClick={scrollToLocation}>
                   {loc.city && (
-                    <span className="location-text">{loc.city}
-                    <span>,</span>
+                    <span className='location-text'>
+                      {loc.city}
+                      <span>,</span>
                     </span>
                   )}
 
                   {loc.country && (
-                    <span className="location-text">{loc.country}<span>,</span></span>
+                    <span className='location-text'>
+                      {loc.country}
+                      <span>,</span>
+                    </span>
                   )}
 
-                  {loc.countryCode && (
-                    <span className="location-text">{loc.countryCode}</span>
-                  )}
-
+                  {loc.countryCode && <span className='location-text'>{loc.countryCode}</span>}
                 </div>
               </div>
-              <div className="details-action-buttons flex">
+              <div className='details-action-buttons flex'>
                 <div>
-                  <button className="share-btn flex align-center">
+                  <button className='share-btn flex align-center'>
                     <GoShare /> Share
                   </button>
                 </div>
                 <div>
-                  <button className="save-btn flex align-center">
+                  <button className='save-btn flex align-center'>
                     <BiHeart /> Save
                   </button>
                 </div>
@@ -116,7 +115,7 @@ export function StayHeader({ name, imgUrls, reviews, loc, host }) {
         </div>
       )}
       {isMobile && (
-        <div className="stay-header-mobile">
+        <div className='stay-header-mobile'>
           <ImageGallery
             items={imgUrls.map((url) => ({ original: url, thumbnail: url }))}
             showPlayButton={false}
@@ -127,18 +126,15 @@ export function StayHeader({ name, imgUrls, reviews, loc, host }) {
             slideDuration={500}
             slideInterval={3000}
           />
-          <div className="stay-header-buttons">
-            <button
-              className="back-btn flex align-center"
-              onClick={() => navigate(-1)}
-            >
+          <div className='stay-header-buttons'>
+            <button className='back-btn flex align-center' onClick={() => navigate(-1)}>
               <PiArrowLeftLight />
             </button>
-            <div className="action-btns flex">
-              <button className="share-btn flex align-center">
+            <div className='action-btns flex'>
+              <button className='share-btn flex align-center'>
                 <PiHeartLight />
               </button>
-              <button className="save-btn flex align-center">
+              <button className='save-btn flex align-center'>
                 <PiUploadSimpleLight />
               </button>
             </div>
@@ -146,7 +142,7 @@ export function StayHeader({ name, imgUrls, reviews, loc, host }) {
         </div>
       )}
     </React.Fragment>
-  );
+  )
 }
 
 {
--- src/cmps/StayDetails/StayReviews.jsx
+++ src/cmps/StayDetails/StayReviews.jsx
@@ -1,55 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { AiFillStar } from "react-icons/ai";
-import { store } from "../../store/store";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react'
+import { AiFillStar } from 'react-icons/ai'
+import { store } from '../../store/store'
+import { useSelector } from 'react-redux'
 
 export function StayReviews({ data }) {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-  const reviewsModal = useSelector(
-    (storeState) => storeState.userModule.reviewsModal
-  );
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
+  const reviewsModal = useSelector((storeState) => storeState.userModule.reviewsModal)
 
-  const maxReviewsToShow = isMobile ? 3 : 6;
-  const slicedReviews = data.slice(0, maxReviewsToShow);
+  const maxReviewsToShow = isMobile ? 3 : 6
+  const slicedReviews = data.slice(0, maxReviewsToShow)
 
-  const hash = window.location.hash;
+  const hash = window.location.hash
 
   useEffect(() => {
     const closeModal = (ev) => {
-      if (ev.target.classList.contains("reviews-modal-container")) {
-        store.dispatch({ type: "SET_REVIEWS_MODAL", reviewsModal: false });
+      if (ev.target.classList.contains('reviews-modal-container')) {
+        store.dispatch({ type: 'SET_REVIEWS_MODAL', reviewsModal: false })
       }
     }
 
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
+      setIsMobile(window.innerWidth < 768)
     }
 
-    window.addEventListener("resize", handleResize);
-    window.addEventListener("click", closeModal);
+    window.addEventListener('resize', handleResize)
+    window.addEventListener('click', closeModal)
     return () => {
-      window.removeEventListener("resize", handleResize);
-      window.removeEventListener("click", closeModal);
-    };
-  }, []);
-  const reviews = data;
+      window.removeEventListener('resize', handleResize)
+      window.removeEventListener('click', closeModal)
+    }
+  }, [])
+  const reviews = data
 
   const sumOfRatings = reviews.reduce((acc, review) => {
-    return acc + review.rate;
-  }, 0);
+    return acc + review.rate
+  }, 0)
 
-  const avgRating = (sumOfRatings / reviews.length).toFixed(2);
+  const rawAvg = sumOfRatings / reviews.length
+  const avgRating = rawAvg % 1 === 0 ? Math.floor(rawAvg) : rawAvg.toFixed(2)
 
   return (
     <React.Fragment>
-      <div
-        className={
-          isMobile ? "main-layout small reviews-container" : "reviews-container"
-        }
-      >
-        <div className="reviews-header flex">
-          <div className="reviews-rating flex">
-            <div className="flex" style={{ placeSelf: "center" }}>
+      <div className={isMobile ? 'main-layout small reviews-container' : 'reviews-container'}>
+        <div className='reviews-header flex'>
+          <div className='reviews-rating flex'>
+            <div className='flex' style={{ placeSelf: 'center' }}>
               <AiFillStar />
             </div>
             <span>{avgRating}</span>
@@ -57,31 +52,29 @@ export function StayReviews({ data }) {
           <span>•</span>
           <span>{reviews.length} Reviews</span>
         </div>
-        <div className="reviews-list">
+        <div className='reviews-list'>
           {slicedReviews.map((review) => (
-            <li className="review flex" key={review.by.id}
-            style={{maxWidth: '80%'}}
-            >
-              <div className="review-title flex">
-                <img src={review.by.imgUrl} alt="" />
-                <div className="reviewer-details flex">
+            <li className='review flex' key={review.by.id} style={{ maxWidth: '80%' }}>
+              <div className='review-title flex'>
+                <img src={review.by.imgUrl} alt='' />
+                <div className='reviewer-details flex'>
                   <span>{review.by.fullname}</span>
                   <span>October 2021</span>
                 </div>
               </div>
-              <div className="review-body">
+              <div className='review-body'>
                 <p>{review.txt}</p>
               </div>
             </li>
           ))}
         </div>
         <div
-          className="show-all-reviews"
+          className='show-all-reviews'
           onClick={() => {
             store.dispatch({
-              type: "SET_REVIEWS_MODAL",
+              type: 'SET_REVIEWS_MODAL',
               reviewsModal: true,
-            });
+            })
           }}
         >
           Show all {reviews.length} reviews
@@ -89,41 +82,48 @@ export function StayReviews({ data }) {
       </div>
 
       {reviewsModal && (
-        <div className="reviews-modal-container">
-          <div className="reviews-modal">
+        <div className='reviews-modal-container'>
+          <div className='reviews-modal'>
             <button
-              className="close-modal-btn"
+              className='close-modal-btn'
               onClick={() => {
                 store.dispatch({
-                  type: "SET_REVIEWS_MODAL",
+                  type: 'SET_REVIEWS_MODAL',
                   reviewsModal: false,
-                });
+                })
               }}
             >
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false"><path d="m6 6 20 20M26 6 6 26"></path></svg>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 32 32'
+                aria-hidden='true'
+                role='presentation'
+                focusable='false'
+              >
+                <path d='m6 6 20 20M26 6 6 26'></path>
+              </svg>
             </button>
             {/* <div className="divider"></div> */}
-            <div className="reviews-modal-list">
-            <h1>Reviews</h1>
-            {reviews.map((review) => (
-              <li className="review flex" key={review.by.id}
-              >
-                <div className="review-title flex">
-                  <img src={review.by.imgUrl} alt="" />
-                  <div className="reviewer-details flex">
-                    <span>{review.by.fullname}</span>
-                    <span>October 2021</span>
+            <div className='reviews-modal-list'>
+              <h1>Reviews</h1>
+              {reviews.map((review) => (
+                <li className='review flex' key={review.by.id}>
+                  <div className='review-title flex'>
+                    <img src={review.by.imgUrl} alt='' />
+                    <div className='reviewer-details flex'>
+                      <span>{review.by.fullname}</span>
+                      <span>October 2021</span>
+                    </div>
                   </div>
-                </div>
-                <div className="review-body">
-                  <p>{review.txt}</p>
-                </div>
-              </li>
-            ))}
+                  <div className='review-body'>
+                    <p>{review.txt}</p>
+                  </div>
+                </li>
+              ))}
             </div>
           </div>
         </div>
       )}
     </React.Fragment>
-  );
+  )
 }
