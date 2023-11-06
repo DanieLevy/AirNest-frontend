872b186b4d3c4981c2a8bb1280673cc556546d0b
Daniel Levy <daniellofficial@gmail.com>
2023-11-07 00:35:51 +0200

    Add react-spinners and update styles and loading animation

--- src/cmps/StayList.jsx
+++ src/cmps/StayList.jsx
@@ -1,16 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import { StayPreview } from './StayPreview'
-import { StayLoader } from './StayLoader'
+import React, { useEffect, useState } from "react";
+import { StayPreview } from "./StayPreview";
+import { StayLoader } from "./StayLoader";
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 export function StayList({ stays, isLoading }) {
-  
-  if (stays.length === 0) return (<div className="no-stays">No stays found</div>)
-  
+  if (isLoading)
+    return (
+      <PropagateLoader
+        color={"#ff385c"}
+        //  size={150}
+        className="loader"
+        speedMultiplier={0.8}
+      />
+    );
+  //  loading={isLoading}
+  if (stays.length === 0)
+    return (
+      <div className="no-stays">
+        <img src="https://i.ibb.co/LvDMwQQ/ok.jpg" alt="no-stays" border="0" />
+        <h1>No Stays Found..</h1>
+        <h3>Try to change your search</h3>
+      </div>
+    );
+
   return (
-    <section className='stay-list'>
+    <section className="stay-list">
       {stays.map((stay) =>
-        isLoading ? <StayLoader key={stay._id} /> : <StayPreview key={stay._id} stay={stay} />
+        isLoading ? (
+          <StayLoader key={stay._id} />
+        ) : (
+          <StayPreview key={stay._id} stay={stay} />
+        )
       )}
     </section>
-  )
+  );
 }
