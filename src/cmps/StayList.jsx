d104b6601378ac659db86e10d731de08d3bfb94a
David <sumdavid15@gmail.com>
2023-11-07 03:17:50 +0200

    Added infinite scrolling

--- src/cmps/StayList.jsx
+++ src/cmps/StayList.jsx
@@ -1,16 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import { StayPreview } from './StayPreview'
-import { StayLoader } from './StayLoader'
+import React, { useEffect, useState } from 'react';
+import { StayPreview } from './StayPreview';
+import { StayLoader } from './StayLoader';
 
 export function StayList({ stays, isLoading }) {
-  
-  if (stays.length === 0) return (<div className="no-stays">No stays found</div>)
-  
+  if (stays.length === 0)
+    return (
+      <div className='no-stays' style={{display:'flex' ,flexDirection:'column',gap:10 ,marginTop:20}}>
+        <h1 style={{fontSize:22}}>No exact matches</h1>
+        <p style={{fontSize:'1rem'}}>Try changing or removing some of your filters or adjusting your search area.</p>
+      </div>
+    );
+
   return (
     <section className='stay-list'>
       {stays.map((stay) =>
         isLoading ? <StayLoader key={stay._id} /> : <StayPreview key={stay._id} stay={stay} />
       )}
     </section>
-  )
+  );
 }
--- src/pages/StayIndex.jsx
+++ src/pages/StayIndex.jsx
@@ -1,34 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
-import { StayList } from "../cmps/StayList.jsx";
-import { loadStays } from "../store/actions/stay.actions.js";
-import { StayMapIndex } from "../cmps/StayMapIndex.jsx";
+import { StayList } from '../cmps/StayList.jsx';
+import { loadStays } from '../store/actions/stay.actions.js';
+import { StayMapIndex } from '../cmps/StayMapIndex.jsx';
 
-import { HiMiniMap } from "react-icons/hi2";
-import { HiMiniListBullet } from "react-icons/hi2";
-import { StayFilter } from "../cmps/StayFilter.jsx";
-import { useSearchParams } from "react-router-dom";
-import { QUERY_KEYS } from "../services/util.service.js";
+import { HiMiniMap } from 'react-icons/hi2';
+import { HiMiniListBullet } from 'react-icons/hi2';
+import { StayFilter } from '../cmps/StayFilter.jsx';
+import { useSearchParams } from 'react-router-dom';
+import { QUERY_KEYS } from '../services/util.service.js';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { StayLoader } from '../cmps/StayLoader.jsx';
 
 export function StayIndex() {
+  const stays = useSelector((storeState) => storeState.stayModule.filteredStays);
+  const isLoading = useSelector((storeState) => storeState.systemModule.isLoading);
+
   const [listMode, setListMode] = useState(true); // true = list, false = map
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   const [isVisible, setIsVisible] = useState(true);
   const [searchParams] = useSearchParams();
 
+  const [displayCount, setDisplayCount] = useState(21);
+  const displayStays = stays.slice(0, displayCount);
+  console.log('displayStays:', displayStays.length)
+
+  useEffect(() => {
+    setDisplayCount(21);
+  }, [searchParams]);
+
+  const fetchMoreData = () => {
+    setDisplayCount(displayCount + 21);
+  };
+
   const region = searchParams.get(QUERY_KEYS.region);
   const adults = searchParams.get(QUERY_KEYS.adults);
   const children = searchParams.get(QUERY_KEYS.children);
   const label = searchParams.get(QUERY_KEYS.label);
-
-  const stays = useSelector(
-    (storeState) => storeState.stayModule.filteredStays
-  );
-
-  const isLoading = useSelector(
-    (storeState) => storeState.systemModule.isLoading
-  );
 
   useEffect(() => {
     loadStays(searchParams);
@@ -47,44 +56,41 @@ export function StayIndex() {
       prevScrollY = currentScrollY;
     };
 
-    window.addEventListener("scroll", handleScroll);
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
 
     return () => {
-      window.removeEventListener("resize", handleResize);
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
   return (
     <React.Fragment>
       <StayFilter />
-      <main className="main-layout stay-index">
+      <main className='main-layout stay-index'>
         {isMobile ? (
           <section>
             <div
-              className={`show-map-btn-container ${
-                isVisible && isMobile ? "" : "hidden"
-              }`}
-              style={{ bottom: isMobile ? "70px" : "90px" }}
+              className={`show-map-btn-container ${isVisible && isMobile ? '' : 'hidden'}`}
+              style={{ bottom: isMobile ? '70px' : '90px' }}
             >
               <button
-                className="show-map-btn"
+                className='show-map-btn'
                 onClick={() =>
                   // scroll to top of page
-                  window.scrollTo({ top: 0, behavior: "smooth" }) &
-                  setListMode(!listMode)
+                  window.scrollTo({ top: 0, behavior: 'smooth' }) & setListMode(!listMode)
                 }
               >
                 {listMode ? (
                   <>
                     Show Map
-                    <HiMiniMap className="map-icon" />
+                    <HiMiniMap className='map-icon' />
                   </>
                 ) : (
                   <>
                     Show List
-                    <HiMiniListBullet className="map-icon" />
+                    <HiMiniListBullet className='map-icon' />
                   </>
                 )}
               </button>
@@ -99,27 +105,32 @@ export function StayIndex() {
           <section>
             <div className={`show-map-btn-container`}>
               <button
-                className="show-map-btn"
+                className='show-map-btn'
                 onClick={() =>
-                  window.scrollTo({ top: 0, behavior: "smooth" }) &
-                  setListMode(!listMode)
+                  window.scrollTo({ top: 0, behavior: 'smooth' }) & setListMode(!listMode)
                 }
               >
                 {listMode ? (
                   <>
                     Show Map
-                    <HiMiniMap className="map-icon" />
+                    <HiMiniMap className='map-icon' />
                   </>
                 ) : (
                   <>
                     Show List
-                    <HiMiniListBullet className="map-icon" />
+                    <HiMiniListBullet className='map-icon' />
                   </>
                 )}
               </button>
             </div>
             {listMode ? (
-              <StayList stays={stays} isLoading={isLoading} />
+              <InfiniteScroll
+                dataLength={displayStays.length}
+                next={fetchMoreData}
+                hasMore={displayCount < stays.length}
+              >
+                {<StayList stays={displayStays} isLoading={isLoading} />}
+              </InfiniteScroll>
             ) : (
               <StayMapIndex stays={stays} />
             )}
