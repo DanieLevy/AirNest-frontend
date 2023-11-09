4096bd0f70ac9b7740be05c56596aa9483b51f4e
Daniel Levy <daniellofficial@gmail.com>
2023-11-09 10:52:04 +0200

    Add dropzone and react-dropzone packages, update upload service to use react-dropzone, and add image preview to StayEdit page

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -1,26 +1,36 @@
 export const uploadService = {
   uploadImg
-}
+};
+
 async function uploadImg(ev) {
   console.log('entered uploadImg');
-  const CLOUD_NAME = "dupxzwzxj"
-  const UPLOAD_PRESET = "fvplrxp8"
-  const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+  const CLOUD_NAME = "dupxzwzxj";
+  const UPLOAD_PRESET = "fvplrxp8";
+  const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
 
   try {
-    const formData = new FormData()
-    formData.append('upload_preset', UPLOAD_PRESET)
-    formData.append('file', ev.target.files[0])
+    const formData = new FormData();
+    formData.append('file', ev);  // Assuming ev is the File object from the dropzone
+    formData.append('upload_preset', UPLOAD_PRESET);  // Include the upload preset
 
-    const res = await fetch(UPLOAD_URL, {
+    const response = await fetch(UPLOAD_URL, {
       method: 'POST',
-      body: formData
-    })
-    const imgUrl = await res.json()
-    return imgUrl
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    // Now 'data' should contain the Cloudinary response
+    console.log('Cloudinary response:', data);
+
+    // Access the secure_url property
+    const secureUrl = data.secure_url;
+    console.log('Secure URL:', secureUrl);
+
+    // Return or do something with the secureUrl
+    return secureUrl;
   } catch (err) {
-    console.error('Failed to upload', err)
-    throw err
+    console.error('Failed to upload', err);
+    throw err;
   }
 }
-
